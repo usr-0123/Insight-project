3c981b98055a637aa6c55e31fe2b9bc14a92065c
lewiskemboi <lewis.kemboi@griffinglobaltech.com>
2025-02-15 16:55:03 +0300

    - Logger formatting and parameters.

--- src/services/send-email.ts
+++ src/services/send-email.ts
@@ -5,13 +5,6 @@ import { logger } from "../utilis/logger";
 // Load environment variables
 dotenv.config();
 
-// Get parameters from CLI arguments
-const recipient = process.argv[2];
-const subject =  process.argv[3];
-const message =  process.argv[4];
-
-console.log( `${message}\n${subject}\n${message}\n` );
-
 // Function to send an email
 export const sendEmail = async (): Promise<void> => {
     try {
@@ -27,9 +20,9 @@ export const sendEmail = async (): Promise<void> => {
         // Email details
         const mailOptions = {
             from: process.env.EMAIL_USER,
-            to: recipient || process.env.EMAIL_RECP || process.env.EMAIL_RECP_DEFAULT,
-            subject: subject || process.env.SUBJECT || "GitHub Actions - Automated Email (TypeScript)",
-            text: message || process.env.MESSAGE || "Hello! This email was sent using GitHub Actions and Nodemailer with TypeScript.",
+            to: process.env.EMAIL_RECP || process.env.EMAIL_RECP_DEFAULT,
+            subject: process.env.SUBJECT || "GitHub Actions - Automated Email (TypeScript)",
+            text: process.env.MESSAGE || "Hello! This email was sent using GitHub Actions and Nodemailer with TypeScript.",
         };
 
         // Send the email
